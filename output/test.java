61e3eaf27794ee01e3aa1e31a7e48813ad09c770
Gausx <55922272+Gausx@users.noreply.github.com>
2021-05-18 14:19:04 +0800

    5.18

--- output/test.java
+++ output/test.java
@@ -1,85 +1,19 @@
-class Solution {
-    public int swimInWater(int[][] grid) {
-        int n = grid.length;
-        int m = grid[0].length;
-
-        List<int[]> list = new ArrayList<>();
-
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                int id = i * m + j;
-
-                if(i < n - 1) {
-                    list.add(new int[]{id, id + m, Math.max(grid[i][j], grid[i + 1][j])});
-                }
-
-                if(j < m - 1) {
-                    list.add(new int[]{id, id + 1, Math.max(grid[i][j], grid[i][j + 1])});
-                }
-            }
-        }
-        Collections.sort(list, new Comparator<int[]>() {
-            public int compare(int[] a, int[] b) {
-                return a[2] - b[2];
-            }
-        });
-
-        Union union = new Union(n * m);
-
-         for(int[] edge : list) {
-            union.connect(edge[0], edge[1]);
-            if(union.connected(0, n * m - 1)) {
-                return edge[2];
-            }
-        }
-        return 0;
-
+public clas DemoPrim {
+    public static void main(String[] args) {
+        //邻接矩阵
+        graph = new int[][]{
+                {0, 6, 1, 5, 0, 0},
+                {6, 0, 5, 0, 3, 0},
+                {1, 5, 0, 5, 6, 4},
+                {5, 0, 5, 0, 0, 2},
+                {0, 3, 6, 0, 0, 6},
+                {0, 0, 4, 2, 6, 0}
+                };
+        int ans = prim(graph, 0);
+        System.out.println(ans);
     }
 
-    class Union {
-        private int count;
-        private int[] parent;
-        private int[] size;
-
-        public Union(int n) {
-            this.count = n;
-            parent = new int[n];
-            size = new int[n];
-            for (int i = 0; i < n; i++) {
-                parent[i] = i;
-                size[i] = 1;
-            }
-        }
-
-        public void connect(int p, int q) {
-            int rootP = find(p);
-            int rootQ = find(q);
-            if(rootP != rootQ) {
-                if(size[rootP] > size[rootQ]) {
-                    parent[rootQ] = rootP;
-                    size[rootP] += size[rootQ];
-                }
-                else {
-                    parent[rootP] = rootQ;
-                    size[rootQ] += size[rootP];
-                }
-            }
-            this.count--;
-        }
-
-        public boolean connected(int p, int q) {
-            return find(p) == find(q);
-        }
-
-        public int find(int x) {
-            while (x != parent[x]) {
-                parent[x] = parent[parent[x]];
-                x = parent[x];
-            }
-            return x;
-        }
-
-
-
+    public static int prime(int[][] graph, int start) {
+        boolean[] via = new boolean[];
     }
 }
